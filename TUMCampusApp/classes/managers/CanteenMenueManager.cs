43a983fb5376e2ebaa84cedf26a7599c12454513
COM8 <sauter.fabian@online.de>
2017-01-20 15:54:29 +0100

    Fixed showing the wrong day

--- TUMCampusApp/classes/managers/CanteenMenueManager.cs
+++ TUMCampusApp/classes/managers/CanteenMenueManager.cs
@@ -87,7 +87,7 @@ namespace TUMCampusApp.classes.managers
             List<CanteenMenu> menus = new List<CanteenMenu>();
             foreach (CanteenMenu m in dB.Query<CanteenMenu>("SELECT * FROM CanteenMenu WHERE typeLong LIKE '%Tagesgericht%'"))
             {
-                if(m.date.CompareTo(time) < 0 && m.date.CompareTo(DateTime.Now) >= -1)
+                if(m.date.Date.CompareTo(time.Date) < 0 && m.date.Date.CompareTo(DateTime.Now.Date) >= 0)
                 {
                     time = m.date;
                 }
