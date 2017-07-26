123b4daee93f7b7139adebebba1c1314259ea178
COM8 <sauter.fabian@mailbox.org>
2017-07-26 17:50:20 +0200

    Added privacy policy and released v.1.0.1

--- TUMCampusAppAPI/Util.cs
+++ TUMCampusAppAPI/Util.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using TUMCampusAppAPI;
 using Windows.UI.Popups;
 
 namespace TUMCampusAppAPI
