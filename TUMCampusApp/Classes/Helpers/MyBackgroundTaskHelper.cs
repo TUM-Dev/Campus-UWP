f5a9350388d0b7f737e74b4bc37e2baa2caecd11
COM8 <sauter.fabian@mailbox.org>
2017-09-30 16:00:47 +0200

    Updated the UWP Community Toolkit

--- TUMCampusApp/Classes/Helpers/MyBackgroundTaskHelper.cs
+++ TUMCampusApp/Classes/Helpers/MyBackgroundTaskHelper.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.Toolkit.Uwp;
+using Microsoft.Toolkit.Uwp.Helpers;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TUMCampusAppAPI;
 using Windows.ApplicationModel.Background;
 
