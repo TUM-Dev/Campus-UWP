f2b7d6b0df5e8bbb7951a00004f58efccdfc94b2
Fabian Sauter <sauter.fabian@mailbox.org>
2021-08-02 16:12:58 +0200

    Calendar jumping to today

--- UI/Pages/Content/CalendarPage.xaml.cs
+++ UI/Pages/Content/CalendarPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using UI_Context.Classes.Context.Pages.Content;
+﻿using System;
+using UI_Context.Classes.Context.Pages.Content;
+using UI_Context.Classes.Templates.Controls.Calendar;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -48,7 +50,20 @@ namespace UI.Pages.Content
 
         private void OnGoToTodayClicked(object sender, RoutedEventArgs e)
         {
-
+            if (VIEW_MODEL.MODEL.EVENTS_COLLECTIONS.IsEmpty())
+            {
+                return;
+            }
+            CalendarEventGroupDataTemplate target = VIEW_MODEL.MODEL.EVENTS_COLLECTIONS[0];
+            foreach (CalendarEventGroupDataTemplate tmp in VIEW_MODEL.MODEL.EVENTS_COLLECTIONS)
+            {
+                if (tmp.Key.Date < DateTime.Now)
+                {
+                    break;
+                }
+                target = tmp;
+            }
+            events_listView.ScrollIntoView(target, ScrollIntoViewAlignment.Leading);
         }
 
         #endregion
