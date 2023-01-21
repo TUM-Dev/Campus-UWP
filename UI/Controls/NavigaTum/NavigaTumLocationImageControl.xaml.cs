0fda83f95eeebf6c1ee159096727b0dcb29dee1f
Fabian Sauter <sauter.fabian@mailbox.org>
2023-01-21 15:51:18 +0100

    Fixed NavigaTUM image author and licenses

--- UI/Controls/NavigaTum/NavigaTumLocationImageControl.xaml.cs
+++ UI/Controls/NavigaTum/NavigaTumLocationImageControl.xaml.cs
@@ -1,7 +1,9 @@
-﻿using ExternalData.Classes.NavigaTum;
+﻿using System;
+using ExternalData.Classes.NavigaTum;
 using UI_Context.Classes.Context.Controls.NavigaTum;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Documents;
 
 namespace UI.Controls.NavigaTum
 {
@@ -33,15 +35,52 @@ namespace UI.Controls.NavigaTum
         #endregion
         //--------------------------------------------------------Misc Methods:---------------------------------------------------------------\\
         #region --Misc Methods (Public)--
-        public void UpdateView(LocationImage image)
+        private void UpdateView(LocationImage image)
         {
             VIEW_MODEL.UpdateView(image);
+
+            UpdateContent(author_tbx, author_border, image.authorName, image.authorUrl);
+            UpdateContent(license_tbx, license_border, image.licenseName, image.licenseUrl);
+
         }
 
         #endregion
 
         #region --Misc Methods (Private)--
+        private static void UpdateContent(TextBlock tbx, Border border, string text, string url)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                border.Visibility = Visibility.Collapsed;
+                return;
+            }
+            border.Visibility = Visibility.Visible;
 
+            tbx.Inlines.Clear();
+            if (string.IsNullOrEmpty(url))
+            {
+                tbx.Inlines.Add(new Run
+                {
+                    Foreground = tbx.Foreground,
+                    Text = text,
+                });
+            }
+            else
+            {
+                tbx.Inlines.Add(new Hyperlink
+                {
+                    Foreground = tbx.Foreground,
+                    Inlines = {
+                        new Run
+                        {
+                            Foreground = tbx.Foreground,
+                            Text = text,
+                        }
+                    },
+                    NavigateUri = new Uri(url)
+                });
+            }
+        }
 
         #endregion
 
@@ -63,6 +102,16 @@ namespace UI.Controls.NavigaTum
                 {
                     control.UpdateView(null);
                 }
+            }
+        }
+
+        private void OnImageSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            // Fixes the unlimited space available since all items a re inside a stack panel.
+            // So we manually update the max width here, based on the image size.
+            if (!double.IsNaN(e.NewSize.Width))
+            {
+                text_grid.MaxWidth = e.NewSize.Width;
             }
         }
 
